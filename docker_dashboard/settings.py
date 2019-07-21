6c1d66556cac03b5b9bb1d488e2170db90046da4
alefeans <alefe.silva@lucree.com.br>
2019-07-21 16:16:47 -0300

    adjusting security items

--- docker_dashboard/settings.py
+++ docker_dashboard/settings.py
@@ -3,18 +3,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-4+bk(vpr-^%$zw9j_i_6y5*_#m4dekpf8-=844pu81mjqz73)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '-4+bk(vpr-^%$zw9j_i_6y5*_#m4dekpf8-=844pu81mjqz73)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
